c68b7c1e3f6b83968f18bda43ae5ad4295bbb15a
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-07-01 15:00:33 +0200

    added the AssemblyInformationalVersion attribute

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,4 +3,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("6.0.0.0")]
 [assembly: AssemblyFileVersion("6.0.0.0")]
-
+// The Product version of the assembly. This is the version you would use when talking to customers or for display on your website (the last custom number will be replaced with the revision/commit hashtag). 
+[assembly: AssemblyInformationalVersion("6.0.0.0")]

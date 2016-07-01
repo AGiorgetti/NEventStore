e3fbe2a5d09dffc2f354a1d3ec65b3944b63dfc2
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-07-01 15:00:33 +0200

    added the AssemblyInformationalVersion attribute

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -2,3 +2,5 @@
 
 [assembly: AssemblyVersion("5.2.0.0")]
 [assembly: AssemblyFileVersion("5.2.0.0")]
+// The Product version of the assembly. This is the version you would use when talking to customers or for display on your website (the last custom number will be replaced with the revision/commit hashtag). 
+[assembly: AssemblyInformationalVersion("5.2.0.0.9b8a0c79fb8d72fbf1c02b1e26130c09b0904338")]
